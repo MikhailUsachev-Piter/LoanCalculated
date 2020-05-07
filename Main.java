7a968b9b7bc78c75d3ac204d5710b7e8baf846df
Mikhail Usachev <63136175+MikhailUsachev-Piter@users.noreply.github.com>
2020-05-07 22:00:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        int amount = 1000000;
+        int month = 36;
+        double percent = 9.99;
+        int mp = (int) service.calculate(amount, month, percent);
+        System.out.println("Ваш емесячный платеж: " + mp);
+    }
+}
