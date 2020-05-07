bcffe5b14b8816cba851ea6cb2c6a152123ba7c1
Mikhail Usachev <63136175+MikhailUsachev-Piter@users.noreply.github.com>
2020-05-07 22:09:06 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public double calculate(int amount, int month, double percent) {
-        double mp = amount*percent/1200*Math.pow((1+percent/1200), month)/(Math.pow((1+percent/1200), month)-1);
+        double mp = amount * percent / 1200 * Math.pow((1 + percent / 1200), month) / (Math.pow((1 + percent / 1200), month) - 1);
         return mp;
     }
 }
